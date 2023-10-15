d272e2b0ae297566ac0e67c9008eec28304a47ae
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-10-15 09:47:21 +0200

    Update FULL_mun_.js

--- FULL_mun_.js
+++ FULL_mun_.js
@@ -2,14 +2,19 @@
 * izpis faz 
 * za FullMunLib()
 */
-function iii(n){
-  function dA( a){ let x =  a<180 ? -(a-90) : -(270-a)
+function iii(n,z){
+  function dA1( a){ let x =  a<180 ? -(a-90) : -(270-a)
     return "="+(a<180 ?"E" :"W")+ ("  "+(x>0?"+":"")+x).slice(-3) +"°"
   }
+  function A(x){ return Math.abs(x)}
+  function dA( a){ let x =  a<180 ? -(a-90) : -(270-a)
+    return "="+(a<180 ?"E" :"W")+ ("  "+(x>0?"⬆":"⬇")+A(x)).slice(-3) +"°"
+  }
+
   let d= moment(field( "dan"+n)).format(" DD.MM. ")
   let u= moment(field( "ura"+n)).format("HH:mm  ")
   let a= field( "azimut"+n) 
-  let da= a==null  ? " ~"  : dA(a)
+  let da= a==null  ? " ~"  :  z==null ?dA(a)  :dA1(a)
   
    return (a==null ? " ❔" :a<180 ?"↗️":"↘️")
    +(d.slice(0,1)=="I" ?"~~.~~.~~" :d ) 
@@ -17,8 +22,7 @@ function iii(n){
    +(a==null ?"  ~" :("  "+a).slice(-4) +"° "+ da )
  }
 // leva poravnava za StatusField
-function ii(x,n){//const n=33
-  return ( iii(x)+" ".repeat(n)).slice(0,n==null?29:n)+".\n"
+function ii(x,n,z){
+  return ( iii(x,z)+" ".repeat(n)).slice(0,n==null?29:n)+".\n"
 }
 //★★ end ★★
-
