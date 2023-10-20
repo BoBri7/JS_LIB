1c85c22ae57141c9e31f233ca4ec6a7c9980ebed
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-10-20 11:30:59 +0200

    Update iStatusLine.js

--- iStatusLine.js
+++ iStatusLine.js
@@ -3,5 +3,5 @@ function iSL(x,n){
   for(let i=0;i<x.length;i++){
     x[i]=(x[i]+" ".repeat(55)).slice(0,n==null?33:n)                         
   }
-  return x.join("\n")                         
+  return x.join("\n")+"\n"                         
 }              
