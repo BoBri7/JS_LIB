aaabc1603f43133d342df77cd30f4a461c878fc8
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-10-25 16:45:09 +0200

    Update iStatusLine.js

--- iStatusLine.js
+++ iStatusLine.js
@@ -1,6 +1,6 @@
 function isl(x,n,z){return iSL(x,n,z) }
 function iSL(x,n,z){
-  if(typeof(n)=="string"){ z=n;n=null }
+  if(typeof(n)=="string"){ z=n;n=33 }
   x=x.split("\n")
   for(let i=0;i<x.length;i++){
     x[i]=(x[i]+" ".repeat(55)).slice(0,n==null?33:n)+(z!=null?z:"")                     
