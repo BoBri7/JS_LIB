3ceb5bf474d81ce3114d6f61d41956d3319cb905
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-05 12:30:06 +0100

    Create Tošl2 saldo.js
    Temp

---
+++ Tošl2 saldo.js
@@ -0,0 +1,69 @@
+/*
+★ Bank <=
+  Vcr. Visa Credit ==]
+  Vdb. Visa Dbt.   ==]=ista?
+  Vpy. Visa prePay
+   Vsav. varčevalni
+★ cash <=  Csh. Cash
+? PM. Magna. ? bencin =X ; el.e. =trajnik <= direkt
+? DC. diners. =X
+*/
+/* 
+* = updSld(e,w)
+* entry
+* write return {[obj]}
+*/
+function updSld(e,wr){ //== 
+  function f(x){return e.field(x)}
+  function fi(x,t){                //= izpis key:value
+     let o= x+":="+  f(x) //+" = " +te(x,t)
+     try { o+= "  =  "+frsc(x,t) }
+     catch (er) {o+= " err"}
+     return o
+  }
+  function frsc(x,t){         //= slice cc tgt
+    switch (t){
+    case 0: return s(x) ;break
+    case 1: return c(x) ;break
+    case 2: return rcn(x) ;break
+    default: return "★?"
+  } }
+  function c(x){ return s(x)=="-" ?-1 :1 }  //= cc +/-1
+  function s(x){ return f(x).slice(-1) }       //= slice
+  function rct(x){ let o=""     //= Saldo.target 4transfer
+      switch (s(x)) {
+      case "C": o ="CASH" ;break
+      case "V": o ="BANK" ;break
+      default : o ="othr" 
+      } return o
+  }
+  // return Void=0  R/P x=t? 1,0
+  function stx(t){ let x=s("st"); return x=="V" ?0:t== null ?1 :t==x ?1 :0 }
+
+  let zn="zn",     //= znesek
+  pr="proj",       //= projekt
+  tr="tr" ,        //= trans +p -o =transf
+  rc="rcn",        //= račun V* C* ...
+  rt ="rcnt",       //= račun => transfer
+  st="st"           //= status Re Plan Void
+  let z= f(zn)*c(pr)*c(tr)*stx(st)
+
+  if(wr > 0) {     //= zapis sald
+      e.set("SALDO", zn* fx("R")  )
+      e.set("PLAN",  zn* fx("P")  )
+      e.set( "CASH", zn *  )
+      e.set( "BANK", zn *  )
+      e.set( "OTHR", zn *  )
+      if(s(tr)==">") {  e.set(rcn(rt), -zn)  }
+  }
+  return [fi(rc,2), //račun 
+          fi(zn,3), 
+          fi(tr,1),
+          fi(rt,3),
+          fi(pr,1) ]
+}
+//============
+
+
+
+
