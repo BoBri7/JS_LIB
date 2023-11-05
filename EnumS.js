8772f3906be205c2c442c48f9b8a3610d3b48b9d
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-05 14:05:00 +0100

    Update EnumS.js

--- EnumS.js
+++ EnumS.js
@@ -1,6 +1,7 @@
 function enumFu(f,n){ f=String(f).split("\n")
   let o=""
+  if(n==null)n=0                    
   for(let i=0;i<f.length;i++){
-      o+=i+" "+f[i]+"\n"
+      o+=(i+n)+" "+f[i]+"\n"
   } return o
 } 
