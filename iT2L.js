c03dfcd2c8dd75e3cb873e22923801dadc4052d4
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-11 09:29:09 +0100

    Update iT2L.js
    Popravki it2l 
    Debug

--- iT2L.js
+++ iT2L.js
@@ -1,4 +1,3 @@
-
 /*
 * iT2L izpis Text v Vrstce
  p=
@@ -44,11 +43,53 @@ function iT2L(x,n,p,z,obj){
        o.push( "[mxL="+m+"]")
    }
    for(let i=0;i<c(o);i++){oi=o[i] //== desna poravnava+z
-      o[i]=oi+" ".repeat(n-oi.length+1)+(z!=null ?z:"")
+      o[i]=(oi+" ".repeat(m)).slice(0,m)
+      if(oi.length>m) o[i]+= "+!"+oi.slice(m)
+      o[i]+=(z!=null ?z:"")
+      try { if(DBG)o[i]+="["+c(oi)+">"+c(o[i]) }  //==t= dolžine vrstic
+      catch(er){}
+   } 
+   return o.join("\n")//+"\no1="+cc+"<"
+}
+/* prev. ver errors!
+function iT2L(x,n,p,z,obj){
+  function mm(x){ return Math.max(m,oi.length) }
+  function c(x){ return x.length }
+  const cc =5
+  p= p>0 ?" ".repeat(p)   :""  
+   //  vrstica.          output mxLL. novaVrstica
+  let oi=(n<0 ?"-":""), o=[] , m=0   , xn=""   
+  n= n!=null ?Math.abs(n) :35 
+  x=x.split(" ")
+  for(let i=0;i<c(x);i++){ xi=x[i]  //vse besede ★
+  if(xi>" "){
+     if( c(oi+xi) < n+5) { oi+=xi+" "}
+     else {
+       if(n-c(oi) > 5 ) {
+          xn="-"+xi.slice(n-c(oi)+cc)+" "//ostanek v novo vrstico
+          oi+=xi.slice(0, n-c(oi)+cc)+"-"     // dopolni vrstico
+       }
+        else  {  xn=xi +" " ;  xi ="" }
+     }
+     if(c(oi) > n || xn >""){    //== zapis vrstice
+        m=mm()
+        o.push(oi)
+        oi=p+xn;  xn=""
+     }
+   }
+   }if(oi.trim()>" "){ o.push(oi), m= mm() } //==pripis ostanka
+   if(obj!=null)o.push(eval(obj))                            //4test variavl v skripti
+   if(o[0].slice(0,1)=="-") {    //== pripis dolžine
+       o[0]= o[0].slice(1) 
+       o.push( "[mxL="+m+"]")
+   }
+   for(let i=0;i<c(o);i++){oi=o[i] //== desna poravnava+z
+      o[i]=(oi+" ".repeat(m).slice-oi.length+1)+(z!=null ?z:"")
       //o[i]+="["+c(oi)+">"+c(o[i])   //==t= dolžine vrstic
    } 
    return o.join("\n")//+"\no1="+cc+"<"
 }
+*/
 //=======
 function iL2L(ListObj, LL, h, z){
   h= h!=null ? h+":" :""
