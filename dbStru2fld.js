5d6e6e2385c0273d4de1d4bcfdd4f1aa53ee6135
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-23 09:07:32 +0100

    Update dbStru2fld.js
    Try/catch

--- dbStru2fld.js
+++ dbStru2fld.js
@@ -5,6 +5,35 @@
    ln   1 write line#  /0= not
  * return zapis + zapiše strukturo baze v polje dbSf
 */ 
+// test dbStru
+function dbStru2fld(dbSf,vv,ln){                             
+  function  s(t,n){ if(n==null) n=15
+    t=String(t)
+    return t+" ".repeat(Math.max(1,n-t.length))+"| "
+  } //=6
+  dbSf=dbSf==null ?"dbStru" :dbSf
+  //= test return "tf="+dbSf+"<"+
+  let db=lib(), dbf=db.fields(), n=dbf.length
+  let dbe=db.entries()[0]   //★ za izpis vrednosti if( vv)
+  let o= (ln>0?"=":"")+"dbStru: "+lib().title
+  for( let ii=0; ii<n ; ii++){ fi=dbf[ii]
+    o+= "\n" 
+    o+= ln>0 ?String(1001+ii).slice(n>99 ?3 :2)+" " :""      
+  //log( String(10001+ii).slice(n>99 ?3 :2)+" "+s(fi)+" /n:"+n)//★★            
+    o+= s(fi)
+    o+= s(String(typeof( dbe.field(fi))).slice(0,3) ,5)                 
+    o+= vv>0 || vv<-1  ?dbe.field(fi) :""     //= zapis vrednosti /vv                   
+  }
+  if(vv >=0) {
+//return "====vv"+vv+"/"+dbSf
+    try { dbe.set(dbSf,o)  } //= write to LastEntry /vv
+    catch (Er){"lib() nima target fielda dbStru"}
+ }
+  return o+"\n========\n"
+}
+//==== end === ★
+
+/* prev ver
 function dbStru2fld(dbSf,vv,ln){                             
   function  s(t,n){ if(n==null) n=15
     t=String(t)
