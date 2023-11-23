3fedfd0605fcd4c14b5482965b53d52283f1b574
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-23 20:38:26 +0100

    Update dbStru2fld.js

--- dbStru2fld.js
+++ dbStru2fld.js
@@ -1,36 +1,59 @@
 /*
  * function dbStru(dfSf,val, ln)
-   dfSf = dbStru) :field to write
-   val  1 write values /0= no >0||<-1=yes -1=ni zapisa dbsf=le returnValue
-   ln   1 write line#  /0= not
+   dfSf = dbStru) :field to write //if šl# sshift parameters <=
+   val  1 write values 
+          /0= no >0||<-1=yes -1=ni zapisa dbsf=le returnValue
+   ln   1 write line#  /0= not // $=> find $ in dbStru
  * return zapis + zapiše strukturo baze v polje dbSf
 */ 
-// test dbStru
+// test dbStru. =★9
 function dbStru2fld(dbSf,vv,ln){                             
   function  s(t,n){ if(n==null) n=15
     t=String(t)
     return t+" ".repeat(Math.max(1,n-t.length))+"| "
-  } //=6
+  }   
+  function u(s){ return s.toUpperCase() }
+ if( ! isNaN(dbSf) ){ ln=vv;  vv=dbSf;  dbSf=null  }  
+   //=★★★★
+  //  return typeof(dbSf)+"===="+vv
+
   dbSf=dbSf==null ?"dbStru" :dbSf
   //= test return "tf="+dbSf+"<"+
   let db=lib(), dbf=db.fields(), n=dbf.length
   let dbe=db.entries()[0]   //★ za izpis vrednosti if( vv)
-  let o= (ln>0?"=":"")+"dbStru: "+lib().title
-  for( let ii=0; ii<n ; ii++){ fi=dbf[ii]
+  let o= (ln>0?"=":"\n")+"dbStru: "+lib().title,pis=o.length
+  for( let ii=0; ii<n ; ii++){ fi=dbf[ii]  //=★★★ zanka ★★★
+    o+= "["+(o.length)+"]"
     o+= "\n" 
-    o+= ln>0 ?String(1001+ii).slice(n>99 ?3 :2)+" " :""      
+    o+= ln>0 ?String(1001+ii).slice(n>99 ?3 :2)+" " :""     
+ 
   //log( String(10001+ii).slice(n>99 ?3 :2)+" "+s(fi)+" /n:"+n)//★★            
     o+= s(fi)
     o+= s(String(typeof( dbe.field(fi))).slice(0,3) ,5)                 
-    o+= vv>0 || vv<-1  ?dbe.field(fi) :""     //= zapis vrednosti /vv                   
-  }
+    o+= vv>0 || vv<-1  ?dbe.field(fi) :""     //= zapis vrednosti /vv  
+
+    if(typeof(ln)=="string") {
+       let iis =u(o).indexOf(u(ln),pis)
+       if(iis > 0){ 
+          o+=  "  $¢"+ln+"/"+iis 
+          //pis=o.length ===>★
+          //o+="/>" +pis+" {"+ln
+        } 
+      }
+      pis=o.length      
+   }
   if(vv >=0) {
 //return "====vv"+vv+"/"+dbSf
-    try { dbe.set(dbSf,o)  } //= write to LastEntry /vv
-    catch (Er){"lib() nima target fielda dbStru"}
+    try { dbe.set(dbSf, o)  } //= write to LastEntry /vv
+    catch (Er){ o ="★ lib() nima target fielda "+dbSf+"★ \n"+o}
  }
-  return o+"\n========\n"
+  return"/ \n \n★" +o+"\n========\n"
 }
+//==== end === ★ TEST:
+/*log("bgn")
+log( dbStru2fld("xx",0,"fc") )
+log("end") 
+*/
 //==== end === ★
 
 /* prev ver
