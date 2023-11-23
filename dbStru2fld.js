74a09c545376af58390677836de1c8476dcaf1e4
Bogdan <43040397+BoBri7@users.noreply.github.com>
2023-11-23 08:41:38 +0100

    Update dbStru2fld.js

--- dbStru2fld.js
+++ dbStru2fld.js
@@ -1,7 +1,7 @@
 /*
  * function dbStru(dfSf,val, ln)
    dfSf = dbStru) :field to write
-   val  1 write values /0= not
+   val  1 write values /0= no >0||<-1=yes -1=ni zapisa dbsf=le returnValue
    ln   1 write line#  /0= not
  * return zapis + zapiše strukturo baze v polje dbSf
 */ 
@@ -19,10 +19,9 @@ function dbStru2fld(dbSf,vv,ln){dbSf=dbSf==null ?"dbStru" :dbSf
   //log( String(10001+ii).slice(n>99 ?3 :2)+" "+s(fi)+" /n:"+n)//★★            
     o+= s(fi)
     o+= s(String(typeof( dbe.field(fi))).slice(0,3) ,5)                 
-    o+= vv>0 ?dbe.field(fi) :""                        
+    o+= vv>0 || vv<-1  ?dbe.field(fi) :""     //= zapis vrednosti /vv                   
   }
-
-  dbe.set(dbSf,o)  // ★tempDisable //write to LastEntry
+  if(vv>=0) dbe.set(dbSf,o)   //= write to LastEntry /vv
   return o+"\n========\n"
 }
 //==== end ===
